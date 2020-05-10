fe75cbfa221355b3163732122532c66839721698
robynp108 <robynp108@gmail.com>
2020-05-09 19:06:56 -0500

    more file name typos

--- public/service-worker.js
+++ public/service-worker.js
@@ -5,8 +5,8 @@ const FILES_TO_CACHE = [
   "/manifest.webmanifest",
   "/styles.css",
   "/db.js",
-  "/icon-192x192.png",
-  "/icon-512x512.png"
+  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png"
 ];
 const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
